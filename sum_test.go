2d7958ba76bf9740af83a49d7471563dbdfc99c4
AnuchitO <anuchit.prasertasng@gmail.com>
2022-06-06 10:54:17 +0700

    add binary Parser

--- sum_test.go
+++ sum_test.go
@@ -12,3 +12,16 @@ func TestSum(t *testing.T) {
 		t.Fatalf("got %d want %d", got, want)
 	}
 }
+
+func TestSumAll(t *testing.T) {
+	// Arrange
+	xs := []int{1, -2, 3}
+
+	// Act
+	got := Sum(xs...)
+
+	// Assert
+	if got != 2 {
+		t.Error("Expected 2, got", got)
+	}
+}
